aa2fdbb7eb407f5aafb5ff801b998a6416009da6
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-09-19 15:40:49 -0500

    refactoring

--- src/CLIMain.groovy
+++ src/CLIMain.groovy
@@ -3,9 +3,9 @@ import commandLineInterface.CommandLineInterface
 /**
  * Created by rweinstein on 9/19/17.
  */
-class testHello {
+class CLIMain {
 
-  public static void main (String[] args) {
+  static void main(String[] args) {
         def cli = new CommandLineInterface()
         cli.runCLI()
     }
